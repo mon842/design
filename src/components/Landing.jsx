b872c4ba72dba3b6557a8e6af2e0305c333141f2
mon842 <sayakbasak842@gmail.com>
2023-09-30 01:13:59 +0530

    up5

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -23,69 +23,72 @@ function Landing() {
     });
 
     return (
-        <div className="wrapper">
-            <div className="blackDiv">
+        <div>
+            <nav>
+                <div className="inlineLanding">
+                    <img
+                        className="appLogo"
+                        src="https://res.cloudinary.com/dfrntsanb/image/upload/f_auto,q_auto/v1/GetSetOA.com/aqecjxdpowshtfr5tper"
+                    />
+                    <h1 className="name">GetSet </h1>
+                    <h1 className="nameGr">OA.com</h1>
+                </div>
 
-                <nav>
-                    <div className="inlineLanding">
-                        <img
-                            className="appLogo"
-                            src="https://res.cloudinary.com/dfrntsanb/image/upload/f_auto,q_auto/v1/GetSetOA.com/aqecjxdpowshtfr5tper"
-                        />
-                        <h1 className="name">GetSet </h1>
-                        <h1 className="nameGr">OA.com</h1>
+                <div>
+                    <button className="regis button" onClick={() => navigator("/auth")}>
+                        REGISTER
+                    </button>
+                </div>
+
+
+            </nav>
+            <div className="wrapper">
+
+                <div className="blackDiv">
+
+                    <div className="typeWriter inline">
+                        <h2>
+                            <span className="getText">Get ,</span>{" "}
+                            <span className="typeWriterH2">{text}</span>
+                            <span className="typeWriterCursor">
+                                <Cursor />
+                            </span>
+                        </h2>
                     </div>
 
-                    <div>
-                        <button className="regis button" onClick={() => navigator("/auth")}>
-                            REGISTER
-                        </button>
+                    <div className="inline top">
+                        <p className="big">Pooling All </p>
+                    </div>
+
+                    <div className="inline">
+                        <p className="big ques"> OA Questions</p>
+                        <p className="small">from</p>
+                    </div>
+
+                    <div className="inline">
+                        <p className="big tier">TIER-1 Colleges</p>
+                    </div>
+
+                    <div className="inline">
+                        <p className="small bottom"> across</p>
+                        <p className="big india">India</p>
                     </div>
 
 
-                </nav>
+                    <button className="startBtn " onClick={() => navigator("/auth")}>
+                        Start Solving
+                    </button>
 
-                <div className="typeWriter inline">
-                    <h2>
-                        <span className="getText">Get ,</span>{" "}
-                        <span className="typeWriterH2">{text}</span>
-                        <span className="typeWriterCursor">
-                            <Cursor />
-                        </span>
-                    </h2>
                 </div>
 
-                <div className="inline top">
-                    <p className="big">Pooling All </p>
+                <div className="heroimg">
+                    <img src="https://fresources.tech/images/home_banner.svg" alt="" />
                 </div>
 
-                <div className="inline">
-                    <p className="big ques"> OA Questions</p>
-                    <p className="small">from</p>
-                </div>
-
-                <div className="inline">
-                    <p className="big tier">TIER-1 Colleges</p>
-                </div>
-
-                <div className="inline">
-                    <p className="small bottom"> across</p>
-                    <p className="big india">India</p>
-                </div>
-
-
-                <button className="startBtn " onClick={() => navigator("/auth")}>
-                    Start Solving
-                </button>
-
+                <Outlet></Outlet>
             </div>
-
-            <div className="heroimg">
-                <img src="https://fresources.tech/images/home_banner.svg" alt="" />
-            </div>
-
-            <Outlet></Outlet>
         </div>
+
     );
 }
 
